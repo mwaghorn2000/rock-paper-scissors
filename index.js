73651dbacb97cfbbdff6e05c65982eb2eeb2abc0
mwaghorn2000 <m.waghorn2000@gmail.com>
2023-02-25 18:53:22 +1100

    modified logic of existing code to make text appear on screen rather than in console

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-const getComputerChoice = () => {
+const playRound = (playerChoice) => {
   const choice = ["rock", "paper", "scissors"];
-  return choice[Math.floor(Math.random() * 3)];
-};
-
-const playRound = (computerChoice) => {
-  let playerChoice = prompt("Enter Rock, Paper or Scissors").toLowerCase();
+  let computerChoice = choice[Math.floor(Math.random() * 3)];
+  console.log(playerChoice);
+  let container = document.querySelector("div");
+  let h2 = document.createElement("h2");
   if (computerChoice === playerChoice) {
-    console.log(`Its a Tie! You both chose ${playerChoice}`);
-    return 0;
+    h2.textContent = `Its a Tie! You both chose ${playerChoice}`;
+    container.appendChild(h2);
   } else if (computerChoice === "rock" && playerChoice === "paper") {
-    console.log(`You Win! Paper beats rock`);
-    return 1;
+    h2.textContent = `You Win! Paper beats rock`;
+    constainer.appendChild(h2);
   } else if (computerChoice === "rock" && playerChoice === "scissors") {
-    console.log(`You Lose! Rock beats scissors`);
-    return 2;
+    h2.textContent = `You Lose! Rock beats scissors`;
+    container.appendChild(h2);
   } else if (computerChoice === "paper" && playerChoice === "rock") {
-    console.log(`You Lose! Paper beats Rock`);
-    return 2;
+    h2.textContent = `You Lose! Paper beats Rock`;
+    container.appendChild(h2);
   } else if (computerChoice === "paper" && playerChoice === "scissors") {
-    console.log(`You Win! Scissors beats paper`);
-    return 1;
+    h2.textContent = `You Win! Scissors beats paper`;
+    container.appendChild(h2);
   } else if (computerChoice === "scissors" && playerChoice === "rock") {
-    console.log(`You Win! Rock beats Scissors`);
-    return 1;
+    h2.textContent = `You Win! Rock beats Scissors`;
+    container.appendChild(h2);
   } else if (computerChoice === "scissors" && playerChoice === "paper") {
-    console.log(`You Lose! Scissors beats Paper`);
-    return 2;
-  } else {
-    console.log("Please enter either Rock, Paper or Scissors. You lose");
-    return 0;
+    h2.textContent = `You Lose! Scissors beats Paper`;
+    container.appendChild(h2);
   }
 };
+
+document.getElementById("rock").addEventListener("click", function () {
+  playRound("rock");
+});
+
+document.getElementById("paper").addEventListener("click", function () {
+  playRound("paper");
+});
+
+document.getElementById("scissors").addEventListener("click", function () {
+  playRound("scissors");
+});
